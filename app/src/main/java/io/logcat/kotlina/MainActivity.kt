90f42f08ed0a02565ff47b571cb4536905b789c7
Kadenge Yaa <kadengeyaa@gmail.com>
2020-06-02 20:10:06 +0300

    Init

---
+++ app/src/main/java/io/logcat/kotlina/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.logcat.kotlina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
